853bf5cf40b52c2b60ef7893d5dce0269ce96873
tom <caolvchong@gmail.com>
2018-01-04 02:29:40 +0800

    eslint

--- index.js
+++ index.js
@@ -1,145 +1,145 @@
 function isArray(obj) {
   if (Array.isArray) {
-    return Array.isArray(obj);
+    return Array.isArray(obj)
   }
   if (Object.prototype.toString.call(obj) === '[object Array]') {
-    return true;
+    return true
   }
-  return false;
+  return false
 }
 
 function shadowClone(source) {
   if (isArray(source)) {
-    return source.slice();
+    return source.slice()
   }
-  return Object.assign({}, source);
+  return Object.assign({}, source)
 }
 
 function setIn(obj, data) {
-  const result = shadowClone(obj);
-  const cache = {};
+  const result = shadowClone(obj)
+  const cache = {}
   if (data) {
     Object.keys(data).forEach(keys => {
-      const arr = keys.split('.');
-      const last = arr.pop();
-      const len = arr.length;
-      let value = data[keys];
-      let pointer;
-      let origin;
+      const arr = keys.split('.')
+      const last = arr.pop()
+      const len = arr.length
+      let value = data[keys]
+      let pointer
+      let origin
       if (len) {
-        let prev;
-        let cacheKey;
+        let prev
+        let cacheKey
         for (let i = 0; i < len; i += 1) {
-          const key = arr[i];
+          const key = arr[i]
           if (i === 0) {
-            cacheKey = key;
+            cacheKey = key
             if (cache[cacheKey] === undefined) {
-              origin = shadowClone(result[key]);
-              cache[cacheKey] = origin;
+              origin = shadowClone(result[key])
+              cache[cacheKey] = origin
             } else {
-              origin = cache[cacheKey];
+              origin = cache[cacheKey]
             }
-            pointer = origin;
-            prev = pointer;
+            pointer = origin
+            prev = pointer
           } else {
-            cacheKey += `.${key}`;
+            cacheKey += `.${key}`
             if (cache[cacheKey] === undefined) {
-              pointer = shadowClone(pointer[key]);
-              cache[cacheKey] = pointer;
+              pointer = shadowClone(pointer[key])
+              cache[cacheKey] = pointer
             } else {
-              pointer = shadowClone(pointer[key]);
+              pointer = shadowClone(pointer[key])
             }
-            prev[key] = pointer;
-            prev = pointer;
+            prev[key] = pointer
+            prev = pointer
           }
         }
         if (typeof value === 'function') {
-          value = value(prev[last]);
+          value = value(prev[last])
         }
-        pointer[last] = value;
-        result[arr[0]] = origin;
+        pointer[last] = value
+        result[arr[0]] = origin
       } else {
         if (typeof value === 'function') {
-          value = value(result[keys]);
+          value = value(result[keys])
         }
-        result[keys] = value;
+        result[keys] = value
       }
-    });
+    })
   }
-  return result;
+  return result
 }
 
 function getIn(obj, path) {
-  let result = obj;
+  let result = obj
   if (path) {
-    const arr = path.split('.');
+    const arr = path.split('.')
     for (let i = 0; i < arr.length; i += 1) {
-      const key = arr[i].trim();
+      const key = arr[i].trim()
       if (result === undefined) {
-        return result;
+        return result
       }
-      result = result[key];
+      result = result[key]
     }
   }
-  return result;
+  return result
 }
 
 function getArray(obj, path) {
-  let arr = getIn(obj, path);
+  let arr = getIn(obj, path)
   if (!isArray(arr)) {
-    throw new Error(`${obj} ${path} is not an array`);
+    throw new Error(`${obj} ${path} is not an array`)
   }
-  arr = shadowClone(arr);
-  return arr;
+  arr = shadowClone(arr)
+  return arr
 }
 
 function setArray(obj, path, arr) {
   if (!path) {
-    return arr;
+    return arr
   }
   const data = {
     [path]: arr
-  };
-  return setIn(obj, data);
+  }
+  return setIn(obj, data)
 }
 
 function push(obj, path, data) {
-  const arr = getArray(obj, path);
-  arr.push(data);
-  return setArray(obj, path, arr);
+  const arr = getArray(obj, path)
+  arr.push(data)
+  return setArray(obj, path, arr)
 }
 
 function pop(obj, path) {
-  const arr = getArray(obj, path);
-  arr.pop();
-  return setArray(obj, path, arr);
+  const arr = getArray(obj, path)
+  arr.pop()
+  return setArray(obj, path, arr)
 }
 
 function shift(obj, path) {
-  const arr = getArray(obj, path);
-  arr.shift();
-  return setArray(obj, path, arr);
+  const arr = getArray(obj, path)
+  arr.shift()
+  return setArray(obj, path, arr)
 }
 
 function unshift(obj, path, data) {
-  const arr = getArray(obj, path);
-  arr.unshift(data);
-  return setArray(obj, path, arr);
+  const arr = getArray(obj, path)
+  arr.unshift(data)
+  return setArray(obj, path, arr)
 }
 
 function splice(obj, path, start, deleteCount, ...add) {
-  const arr = getArray(obj, path);
+  const arr = getArray(obj, path)
   if (deleteCount === undefined) {
-    deleteCount = arr.length - start;
+    deleteCount = arr.length - start
   }
-  arr.splice(start, deleteCount, ...add);
-  return setArray(obj, path, arr);
+  arr.splice(start, deleteCount, ...add)
+  return setArray(obj, path, arr)
 }
 
-exports.setIn = setIn;
-exports.getIn = getIn;
-exports.push = push;
-exports.pop = pop;
-exports.shift = shift;
-exports.unshift = unshift;
-exports.splice = splice;
+exports.setIn = setIn
+exports.getIn = getIn
+exports.push = push
+exports.pop = pop
+exports.shift = shift
+exports.unshift = unshift
+exports.splice = splice
--- test/array.test.js
+++ test/array.test.js
@@ -1,7 +1,7 @@
-const assert = require('assert');
-const ZI = require('../index');
+const assert = require('assert')
+const ZI = require('../index')
 
-const { setIn, push, pop, shift, unshift, splice } = ZI;
+const { setIn, push, pop, shift, unshift, splice } = ZI
 
 const o1 = [
   {
@@ -30,23 +30,23 @@ const o1 = [
       }
     ]
   }
-];
+]
 
-const OUTER_LENGTH = o1.length;
-const INNER_LENGTH = o1[2].friends.length;
+const OUTER_LENGTH = o1.length
+const INNER_LENGTH = o1[2].friends.length
 
 describe('数组的操作 - setIn', () => {
   it('修改数组中的某个值', () => {
-    const o2 = setIn(o1, { '1.age': 30 });
-    assert.equal(o1[1].age, 25);
-    assert.equal(o2[1].age, 30);
-  });
+    const o2 = setIn(o1, { '1.age': 30 })
+    assert.equal(o1[1].age, 25)
+    assert.equal(o2[1].age, 30)
+  })
   it('修改深层级的数组中的某个值', () => {
-    const o2 = setIn(o1, { '2.friends.0.age': 4 });
-    assert.equal(o1[2].friends[0].age, 3);
-    assert.equal(o2[2].friends[0].age, 4);
-  });
-});
+    const o2 = setIn(o1, { '2.friends.0.age': 4 })
+    assert.equal(o1[2].friends[0].age, 3)
+    assert.equal(o2[2].friends[0].age, 4)
+  })
+})
 
 describe('数组的操作 - push', () => {
   it('往数组 push 一个值', () => {
@@ -54,128 +54,128 @@ describe('数组的操作 - push', () => {
       name: 'green',
       age: 28,
       admin: true
-    });
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o2.length, OUTER_LENGTH + 1);
-    assert.equal(o2[3].admin, true);
-    assert.equal(o2[3].age, 28);
-  });
+    })
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o2.length, OUTER_LENGTH + 1)
+    assert.equal(o2[3].admin, true)
+    assert.equal(o2[3].age, 28)
+  })
   it('往深层级的数组 push 一个值', () => {
     const o2 = push(o1, '2.friends', {
       name: 'blue',
       age: 11
-    });
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o2[2].friends.length, INNER_LENGTH + 1);
-    assert.equal(o2[2].friends[INNER_LENGTH].age, 11);
-  });
-});
+    })
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o2[2].friends.length, INNER_LENGTH + 1)
+    assert.equal(o2[2].friends[INNER_LENGTH].age, 11)
+  })
+})
 
 describe('数组的操作 - pop', () => {
   it('往数组 pop 一个值', () => {
-    const o2 = pop(o1);
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o2.length, OUTER_LENGTH - 1);
-  });
+    const o2 = pop(o1)
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o2.length, OUTER_LENGTH - 1)
+  })
   it('往深层级的数组 pop 一个值', () => {
-    const o2 = pop(o1, '2.friends');
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o2[2].friends.length, INNER_LENGTH - 1);
-    assert.equal(o1[2].friends[0], o2[2].friends[0]);
-  });
-});
+    const o2 = pop(o1, '2.friends')
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o2[2].friends.length, INNER_LENGTH - 1)
+    assert.equal(o1[2].friends[0], o2[2].friends[0])
+  })
+})
 
 describe('数组的操作 - unshift', () => {
   it('往数组 unshift 一个值', () => {
-    const o2 = unshift(o1, '', { name: 'yellow' });
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o2.length, OUTER_LENGTH + 1);
-    assert.equal(o2[0].name, 'yellow');
-    assert.equal(o1[0], o2[1]);
-  });
+    const o2 = unshift(o1, '', { name: 'yellow' })
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o2.length, OUTER_LENGTH + 1)
+    assert.equal(o2[0].name, 'yellow')
+    assert.equal(o1[0], o2[1])
+  })
   it('往深层级的数组 unshift 一个值', () => {
-    const o2 = unshift(o1, '2.friends', { name: 'pink' });
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o2[2].friends.length, INNER_LENGTH + 1);
-    assert.equal(o2[2].friends[0].name, 'pink');
-    assert.equal(o1[2].friends[0], o2[2].friends[1]);
-  });
-});
+    const o2 = unshift(o1, '2.friends', { name: 'pink' })
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o2[2].friends.length, INNER_LENGTH + 1)
+    assert.equal(o2[2].friends[0].name, 'pink')
+    assert.equal(o1[2].friends[0], o2[2].friends[1])
+  })
+})
 
 describe('数组的操作 - shift', () => {
   it('往数组 shift 一个值', () => {
-    const o2 = shift(o1);
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o2.length, OUTER_LENGTH - 1);
-    assert.equal(o1[1], o2[0]);
-  });
+    const o2 = shift(o1)
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o2.length, OUTER_LENGTH - 1)
+    assert.equal(o1[1], o2[0])
+  })
   it('往深层级的数组 shift 一个值', () => {
-    const o2 = shift(o1, '2.friends');
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o2[2].friends.length, INNER_LENGTH - 1);
-    assert.equal(o1[2].friends[1], o2[2].friends[0]);
-  });
-});
+    const o2 = shift(o1, '2.friends')
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o2[2].friends.length, INNER_LENGTH - 1)
+    assert.equal(o1[2].friends[1], o2[2].friends[0])
+  })
+})
 
 describe('数组的操作 - splice', () => {
   it('对数组 splice 操作（只删除）', () => {
-    const o2 = splice(o1, '', 1);
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o2.length, 1);
-    assert.equal(o1[0], o2[0]);
+    const o2 = splice(o1, '', 1)
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o2.length, 1)
+    assert.equal(o1[0], o2[0])
 
-    const o3 = splice(o1, '', 1, 1);
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o3.length, OUTER_LENGTH - 1);
-    assert.equal(o1[0], o3[0]);
-    assert.equal(o1[2], o3[1]);
-  });
+    const o3 = splice(o1, '', 1, 1)
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o3.length, OUTER_LENGTH - 1)
+    assert.equal(o1[0], o3[0])
+    assert.equal(o1[2], o3[1])
+  })
   it('对数组 splice 操作（有删除，有插入）', () => {
-    const o2 = splice(o1, '', 1, 1, { name: 'black' }, { name: 'white' });
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o2.length, OUTER_LENGTH + 1);
-    assert.equal(o1[0], o2[0]);
-    assert.equal(o1[2], o2[3]);
-    assert.equal(o2[1].name, 'black');
-    assert.equal(o2[2].name, 'white');
-  });
+    const o2 = splice(o1, '', 1, 1, { name: 'black' }, { name: 'white' })
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o2.length, OUTER_LENGTH + 1)
+    assert.equal(o1[0], o2[0])
+    assert.equal(o1[2], o2[3])
+    assert.equal(o2[1].name, 'black')
+    assert.equal(o2[2].name, 'white')
+  })
   it('对数组 splice 操作（只插入）', () => {
-    const o2 = splice(o1, '', 1, 0, { name: 'black' }, { name: 'white' });
-    assert.equal(o1.length, OUTER_LENGTH);
-    assert.equal(o2.length, OUTER_LENGTH + 2);
-    assert.equal(o1[0], o2[0]);
-    assert.equal(o1[1], o2[3]);
-    assert.equal(o2[1].name, 'black');
-    assert.equal(o2[2].name, 'white');
-  });
+    const o2 = splice(o1, '', 1, 0, { name: 'black' }, { name: 'white' })
+    assert.equal(o1.length, OUTER_LENGTH)
+    assert.equal(o2.length, OUTER_LENGTH + 2)
+    assert.equal(o1[0], o2[0])
+    assert.equal(o1[1], o2[3])
+    assert.equal(o2[1].name, 'black')
+    assert.equal(o2[2].name, 'white')
+  })
   it('对深层级数组 splice 操作（只删除）', () => {
-    const o2 = splice(o1, '2.friends', 1);
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o2[2].friends.length, INNER_LENGTH - 2);
-    assert.equal(o1[2].friends[0], o2[2].friends[0]);
+    const o2 = splice(o1, '2.friends', 1)
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o2[2].friends.length, INNER_LENGTH - 2)
+    assert.equal(o1[2].friends[0], o2[2].friends[0])
 
-    const o3 = splice(o1, '2.friends', 1, 1);
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o3[2].friends.length, INNER_LENGTH - 1);
-    assert.equal(o1[2].friends[0], o3[2].friends[0]);
-    assert.equal(o1[2].friends[2], o3[2].friends[1]);
-  });
+    const o3 = splice(o1, '2.friends', 1, 1)
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o3[2].friends.length, INNER_LENGTH - 1)
+    assert.equal(o1[2].friends[0], o3[2].friends[0])
+    assert.equal(o1[2].friends[2], o3[2].friends[1])
+  })
   it('对深层次数组 splice 操作（有删除，有插入）', () => {
-    const o2 = splice(o1, '2.friends', 1, 1, { name: 'black' }, { name: 'white' });
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o2[2].friends.length, INNER_LENGTH + 1);
-    assert.equal(o1[2].friends[0], o2[2].friends[0]);
-    assert.equal(o1[2].friends[2], o2[2].friends[3]);
-    assert.equal(o2[2].friends[1].name, 'black');
-    assert.equal(o2[2].friends[2].name, 'white');
-  });
+    const o2 = splice(o1, '2.friends', 1, 1, { name: 'black' }, { name: 'white' })
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o2[2].friends.length, INNER_LENGTH + 1)
+    assert.equal(o1[2].friends[0], o2[2].friends[0])
+    assert.equal(o1[2].friends[2], o2[2].friends[3])
+    assert.equal(o2[2].friends[1].name, 'black')
+    assert.equal(o2[2].friends[2].name, 'white')
+  })
   it('对深层次数组 splice 操作（只插入）', () => {
-    const o2 = splice(o1, '2.friends', 1, 0, { name: 'black' }, { name: 'white' });
-    assert.equal(o1[2].friends.length, INNER_LENGTH);
-    assert.equal(o2[2].friends.length, INNER_LENGTH + 2);
-    assert.equal(o1[2].friends[0], o2[2].friends[0]);
-    assert.equal(o1[2].friends[1], o2[2].friends[3]);
-    assert.equal(o2[2].friends[1].name, 'black');
-    assert.equal(o2[2].friends[2].name, 'white');
-  });
-});
+    const o2 = splice(o1, '2.friends', 1, 0, { name: 'black' }, { name: 'white' })
+    assert.equal(o1[2].friends.length, INNER_LENGTH)
+    assert.equal(o2[2].friends.length, INNER_LENGTH + 2)
+    assert.equal(o1[2].friends[0], o2[2].friends[0])
+    assert.equal(o1[2].friends[1], o2[2].friends[3])
+    assert.equal(o2[2].friends[1].name, 'black')
+    assert.equal(o2[2].friends[2].name, 'white')
+  })
+})
